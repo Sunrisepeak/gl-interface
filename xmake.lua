c81926dbc33144f6dace1408843ec68365f37951
SPeak <speakshen@163.com>
2024-06-25 07:32:24 +0800

    update api and xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -14,11 +14,3 @@ target("gli-opengl.static")
     set_kind("static")
     add_files("backends/gli_opengl.cpp")
     add_packages("glad", "glm")
-
--- gli-opengl-examples
-includes("examples/xmake.lua")
-
--- gli demos
-includes("demos/xmake.lua")
-
--- TODO: rust-ffi bindgen --with-derive-default --output tmp.rs gl_interface.h
